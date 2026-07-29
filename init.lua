bda0980f0ae970c197dc5cfe0493a5bdfc387072
Collin Barker <97cweb@gmail.com>
2026-07-29 16:17:53 -0400

    fix clangd

--- init.lua
+++ init.lua
@@ -152,4 +152,5 @@ vim.keymap.set("n", "gd", vim.lsp.buf.definition)
 vim.keymap.set("n", "gr", vim.lsp.buf.references)
 vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename)
 vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action)
-
+vim.opt.exrc = true
+vim.opt.secure = true
